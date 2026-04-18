6de7865d154909d975e6ef44a3fb369ba3ca6c48
stevecherry28 <s33605@pjwstk.edu.pl>
2026-04-19 00:04:21 +0200

    Dodanie klasy Programista (subclass, superclass - Pracownik). Uwzględnienie dziedziczenia pól, metod. Stworzenie getterów (w tym w adnotacja do getterów klasy-rodzic w toString() ) nadpis metod obliczKosztMiesieczny i przedstawSie. Dodanie metody wypiszTechnologie. I finalnie: nadpis toString na potrzeby klasy Programista

---
+++ software-house/src/Programista.java
@@ -0,0 +1,46 @@
+public class Programista extends Pracownik {
+    private String glownyJezyk;
+    private int liczbaRepozytoriow;
+
+    public Programista(String idPracownika, String imie,
+                       String nazwisko, double stawkaBazowa, String glownyJezyk, int liczbaRepozytoriow) {
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+        this.glownyJezyk = glownyJezyk;
+        this.liczbaRepozytoriow = liczbaRepozytoriow;
+    }
+
+    public String getGlownyJezyk() {
+        return glownyJezyk;
+    }
+
+    public int getLiczbaRepozytoriow() {
+        return liczbaRepozytoriow;
+    }
+
+    @Override
+    public double obliczKosztMiesieczny() {
+        return super.obliczKosztMiesieczny() + 4000;
+    }
+
+    @Override
+    public String przedstawSie() {
+        return super.przedstawSie() + ", PROGRAMISTA";
+    }
+
+    public void wypiszTechnologie() {
+        System.out.println("Główny język: " + '\'' + getGlownyJezyk() + '\''
+                + "\nInne języki: 'Python', 'SQL'");
+    }
+
+    @Override
+    public String toString() {
+        return "Programista{" +
+                "idPracownika='" + pobierzIdPracownika() + '\'' +
+                ", imie='" + getImie() + '\'' +
+                ", nazwisko='" + getNazwisko() + '\'' +
+                ", stawkaBazowa=" + getStawkaBazowa() +
+                ", glownyJezyk= '" + getGlownyJezyk() + '\'' +
+                ", liczbaRepozytoriow=" + getLiczbaRepozytoriow() +
+                '}';
+    }
+}
