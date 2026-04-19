c28bde6c2fbdc287edede4c4c121f0cc6b7c6665
stevecherry28 <s33605@pjwstk.edu.pl>
2026-04-19 23:12:06 +0200

    Pomyłka - nie dodany był plik. Finalne dodanie klasy ZespolProjektowy.

---
+++ software-house/src/ZespolProjektowy.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class ZespolProjektowy {
+    private String nazwaProjektu;
+    private ArrayList<Pracownik> pracownicy;
+
+    public ZespolProjektowy (String nazwaProjektu) {
+        this.nazwaProjektu = nazwaProjektu;
+        this.pracownicy = new ArrayList<>();
+    }
+
+    public String getNazwaProjektu() {
+        return nazwaProjektu;
+    }
+
+    public ArrayList<Pracownik> getPracownicy() {
+        return this.pracownicy;
+    }
+
+    public void dodajPracownika(Pracownik pracownik) {
+        this.pracownicy.add(pracownik);
+    }
+
+    public void wypiszSkladZespolu() {
+        System.out.println("--------Skład zespołu: " + this.nazwaProjektu + "--------");
+        for (Pracownik pracownik : getPracownicy() ) {
+            System.out.println(pracownik.przedstawSie());
+        }
+    }
+
+    public double policzLacznyKoszt() {
+        double tempLacznyKoszt = 0;
+
+        for (Pracownik pracownik : getPracownicy()) {
+            tempLacznyKoszt += pracownik.obliczKosztMiesieczny();
+        }
+        return tempLacznyKoszt;
+    }
+
+    public Pracownik znajdzPoId(String idPracownika) {
+        for (Pracownik pracownik : getPracownicy()) {
+            if (pracownik.pobierzIdPracownika().equals(idPracownika)) {
+               return pracownik;
+            }
+        }
+        return null;
+    }
+
+    public void wypiszProgramistow() {
+        System.out.println("--------Lista programistów--------" );
+        for (Pracownik pracownik : getPracownicy()) {
+            if (pracownik instanceof Programista) {
+                System.out.println(pracownik.przedstawSie());
+            }
+        }
+    }
+
+
+}
